e357b3de485de13d743cdcc4fe62e9088fd973e9
Matthew Alexander <matt.bryantalex@gmail.com>
2021-03-03 13:01:08 -0800

    finished methods

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -23,31 +23,43 @@ namespace WebAPISample.Controllers
         public IActionResult Get(string movie)
         {
             // Retrieve all movies from db logic
-            var movies = _context.Movies.ToList();
-            return Ok(new string[] { "movie1 string", "movie2 string" });
+            var movies = _context.Movies.ToList(); //added
+            return Ok(movies); //added
+            ;
+            //return Ok(new string[] { "movie1 string", "movie2 string" }); --commented out, oc
         }
 
         // GET api/movie/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
+            var movie = _context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
+            return Ok(movie);
+        }
             // Retrieve movie by id from db logic
             // return Ok(movie);
-            return Ok();
-        }
+            //return Ok(); -- commented out, oc
+        
 
         // POST api/movie
         [HttpPost]
-        public IActionResult Post([FromBody]Movie value)
+        public IActionResult Post([FromBody]Movie movie) //changed from Movie value
         {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
             // Create movie in db logic
             return Ok();
         }
 
         // PUT api/movie
         [HttpPut]
-        public IActionResult Put([FromBody] Movie movie)
+        public IActionResult Put(int id, [FromBody] Movie movie) //added parameter int id
         {
+            var movieInDb = _context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
+            movieInDb.MovieId = movie.MovieId;
+            movieInDb.Title = movie.Title;
+            movieInDb.Director = movie.Director;
+            movieInDb.Genre = movie.Genre;
             // Update movie in db logic
             return Ok();
         }
@@ -57,6 +69,7 @@ namespace WebAPISample.Controllers
         public IActionResult Delete(int id)
         {
             // Delete movie from db logic
+            var deleteMovies = _context.Movies.Where(m => m.MovieId == id).Single();
             return Ok();
         }
     }
