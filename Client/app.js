505193fea59b68c9d2d7c7306aacfdee24b0705b
stephon nelson <stephon.nelson81@gmail.com>
2021-03-04 13:39:53 -0600

    Nevin group standup

--- Client/app.js
+++ Client/app.js
@@ -7,26 +7,27 @@
 
 
 
-        $(function () {
-            $.get("https://localhost:44325/api/movie", function (data) {
-                console.log(data);
-                data.map(function (el) {
-                    $("#PrintHere").append(`<div>
-            <div>Title: ${el.title}</div>
-            <div>Director: ${el.director}</div>
-            <div>Genre: ${el.genre}</div>
-            </div><br>`);
-                })
-                data.map(function (el) {
-                    $("#tableData").append(`<tr>
-            <td style="color:red">${el.title}</td>
-            <td>${el.director}</td>
-            <td>${el.genre}</td>
-            </tr>`);
-                })
-            });
-        })
+        
     }
 
     $('#my-form').submit( processForm );
 })(jQuery);
+$(function () {
+    $.get("https://localhost:44325/api/movie", function (data) {
+        console.log(data);
+        data.map(function (el) {
+            $("#PrintHere").append(`<div>
+            <div>Title: ${el.title}</div>
+            <div>Director: ${el.director}</div>
+            <div>Genre: ${el.genre}</div>
+            </div><br>`);
+        })
+        data.map(function (el) {
+            $("#TableData").append(`<tr>
+            <td style="color:red">${el.title}</td>
+            <td>${el.director}</td>
+            <td>${el.genre}</td>
+            </tr>`);
+        })
+    });
+})
