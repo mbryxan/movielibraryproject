daea47fe1eefc93ab2f73e470282b2ab9da39a8f
stephon nelson <stephon.nelson81@gmail.com>
2021-03-05 13:30:30 -0600

    added app.js

--- Client/app.js
+++ Client/app.js
@@ -5,9 +5,6 @@
         	Director: this["director"].value
         };
 
-
-
-        
     }
 
     $('#my-form').submit( processForm );
@@ -15,13 +12,6 @@
 $(function () {
     $.get("https://localhost:44325/api/movie", function (data) {
         console.log(data);
-        data.map(function (el) {
-            $("#PrintHere").append(`<div>
-            <div>Title: ${el.title}</div>
-            <div>Director: ${el.director}</div>
-            <div>Genre: ${el.genre}</div>
-            </div><br>`);
-        })
         data.map(function (el) {
             $("#TableData").append(`<tr>
             <td style="color:red">${el.title}</td>
